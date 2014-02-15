a348fb9cb3a63a8d4197dbdd09c568dfa9223243
Bret K. Ikehara <bret.k.ikehara@gmail.com>
2014-02-15 01:16:38 -1000

    Removed reference to old library.

--- tasks/options.js
+++ tasks/options.js
@@ -5,11 +5,6 @@
  * Copyright (c) 2013 Bret K. Ikehara
  * Licensed under the MIT license.
  */
-var REGEX_TEMPLATE_PATTERN = /^.*\/template[-\\\/](.+)\.(hbs|handlebars|js|html)/i,
-    REGEX_PARTIAL_PATTERN = /^.*\/partial[-\\\/](.+)\.(hbs|handlebars|js)/i,
-    REGEX_HELPER_PATTERN = /^.*\/helper[-\\\/](.+)\.(hbs|handlebars|js)/i,
-    REGEX_PATH_SEPARATOR_PATTERN = /[\\\/]/g;
-
 module.exports = {
     get: function(userOpt) {
         return this.getOptions(userOpt || {}, this.getDefaultOptions());
@@ -39,33 +34,7 @@ module.exports = {
         return obj;
     },
     getDefaultOptions: function() {
-        var os = require('os');
         return {
-            separator: os.EOL + os.EOL,
-            templatePattern: REGEX_TEMPLATE_PATTERN,
-            partialPattern: REGEX_PARTIAL_PATTERN,
-            helperPattern: REGEX_HELPER_PATTERN,
-            packagePattern: REGEX_PATH_SEPARATOR_PATTERN,
-            packageSeparator: '.',
-            'helper-template-name': function(filepath) {
-                return filepath.replace(this.templatePattern, "$1");
-            },
-            'helper-helper-name': function(filepath) {
-                return filepath.replace(this.helperPattern, "$1");
-            },
-            'helper-partial-name': function(name) {
-                return name;
-            },
-            createPackage: function (filepath) {
-                return filepath.replace(this.packagePattern, this.packageSeparator);
-            },
-            opts: {
-                namespace: 'JST'
-            },
-            helperFile: __dirname + '/template/helper.js',
-            partialFile: __dirname + '/template/partial.js',
-            templateFile: __dirname + '/template/template.js',
-            wrapperFile: __dirname + '/template/wrapper.js'
         };
     }   
 };
