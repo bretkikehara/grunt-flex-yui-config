02599fc4c2bddf1000c797a3e316302b3d724420
Bret K. Ikehara <bret.k.ikehara@gmail.com>
2014-02-15 02:22:25 -1000

    Added the YUI config task.

--- tasks/yui.js
+++ tasks/yui.js
@@ -10,11 +10,12 @@ module.exports = function(grunt) {
     "use strict";
 
     var liboptions = require(__dirname + '/options.js'),
+        libprecompile = require(__dirname + '/precompile.js'),
         libfilter = require(__dirname + '/filter.js');
 
     grunt.registerMultiTask(
-        'yui-group',
-        'Generates a config file for a custom YUI group.',
+        'yui-meta',
+        'Generates a meta file for a custom YUI group.',
         function() {
             var options = liboptions.get(this.options()),
                 filter = libfilter.get(grunt, options);
@@ -43,6 +44,44 @@ module.exports = function(grunt) {
 
                     // Write joined contents to destination filepath.
                     grunt.file.write(file.dest, JSON.stringify(metaConfig, null, options.space));
+                }, this);
+            }
+            else {
+                grunt.verbose.writeln('Failed to find files');
+            }
+        }
+    );
+
+    grunt.registerMultiTask(
+        'yui-config',
+        'Generates a custom YUI group config.',
+        function() {
+            var options = liboptions.get(this.options()),
+                precompiler = libprecompile.get(grunt, options),
+                filter = libfilter.get(grunt, options);
+
+            grunt.verbose.writeflags(options, 'Options');
+
+            grunt.verbose.writeln('Reading configs from %s files', this.files.length);
+
+            /**
+            * Run the program.
+            */
+            if (this.files && this.files.length > 0) {
+                this.files.forEach(function(file) {
+                    var meta,
+                        configContent;
+
+                    // store the meta data in the modules object.
+                    meta = file.src.map(function(filepath) {
+                        return grunt.file.read(filepath);
+                    }, this).join('');
+
+                    // wrap meta in the config.
+                    configContent = precompiler.wrap(meta);
+
+                    // Write joined contents to destination filepath.
+                    grunt.file.write(file.dest, configContent);
                 }, this);
             }
             else {
