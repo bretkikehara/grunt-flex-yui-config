1a2ced3e4ff39cb585d233e2a0e6242e5765569a
Bret K. Ikehara <bret.k.ikehara@gmail.com>
2014-02-18 15:33:06 -1000

    Added yui-build multitask.

--- tasks/yui.js
+++ tasks/yui.js
@@ -11,7 +11,9 @@ module.exports = function(grunt) {
 
     var liboptions = require(__dirname + '/options.js'),
         libprecompile = require(__dirname + '/precompile.js'),
-        libfilter = require(__dirname + '/filter.js');
+        libfilter = require(__dirname + '/filter.js'),
+        libpath = require('path'),
+        libutil = require('util');
 
     grunt.registerMultiTask(
         'yui-meta',
@@ -84,6 +86,60 @@ module.exports = function(grunt) {
 
                     // Write joined contents to destination filepath.
                     grunt.file.write(file.dest, configContent);
+                }, this);
+            }
+            else {
+                grunt.verbose.writeln('Failed to find files');
+            }
+        }
+    );
+
+    grunt.registerMultiTask(
+        'yui-build',
+        'Builds each module',
+        function() {
+            var options = liboptions.get(this.options()),
+                precompiler = libprecompile.get(grunt, options),
+                filter = libfilter.get(grunt, options),
+                modules = {};
+
+            grunt.verbose.writeflags(options, 'Options');
+
+            grunt.verbose.writeln('Reading configs from %s files', this.files.length);
+
+            /**
+            * Run the program.
+            */
+            if (this.files && this.files.length > 0) {
+                this.files.forEach(function(file) {
+
+                    file.src.forEach(function(buildFile) {
+                        var build;
+
+                        buildFile = libpath.join(file.cwd, buildFile);
+
+                        grunt.log.writeln('Reading build: %s', buildFile);
+                        build = JSON.parse(grunt.file.read(buildFile));
+
+                        // loop through modules to build
+                        Object.keys(build.builds).forEach(function(moduleName) {
+                            var content = [],
+                                destPath;
+
+                            // join the js files.
+                            build.builds[moduleName].jsfiles.forEach(function(jsfile) {
+                                var data = grunt.file.read(libpath.join(libpath.dirname(buildFile), 'js', jsfile));
+                                grunt.log.writeln('Reading module: %s', jsfile);
+                                content.push(data);
+                            });
+
+                            destPath = libpath.join(file.dest, moduleName, moduleName + '.js');
+
+                            // Write joined contents to destination filepath.
+                            grunt.log.writeln('Writing file: %s', destPath);
+                            grunt.file.write(destPath, content.join('\n'));
+                        });
+                    }, this);
                 }, this);
             }
             else {
