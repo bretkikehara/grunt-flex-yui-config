48c98e7c0c577a91900534ba9fa837aac8820e69
Bret K. Ikehara <bret.k.ikehara@gmail.com>
2014-02-21 00:15:44 -1000

    Refactored the yui tasks to better support building.

--- tasks/yui.js
+++ tasks/yui.js
@@ -60,126 +60,40 @@ module.exports = function(grunt) {
             moduleWrapper: __dirname + '/template/module-wrapper.hbs'
         };
 
-    grunt.registerMultiTask(
-        'yui-meta',
-        'Generates a meta file for a custom YUI group.',
+
+    grunt.registerTask('yui-config', function() {
+        var done = this.async();
+
+        setTimeout(function() {
+            var files,
+                configContent;
+
+            // read the config
+            files = grunt.file.expand({
+                cwd: options.srcDir
+            }, '**/meta/*.json');
+
+            metaConfig.read(options.srcDir, files);
+
+            // wrap meta in the config.
+            configContent = template.wrapConfig({
+                meta: metaConfig.toString(options.spaces)
+            });
+            grunt.file.write(options.buildDir + '/config.js', configContent);
+            done();
+        }, 100);
+    });
+
+    grunt.registerTask('yui',
+        'Defines the YUI task',
         function() {
-            var options = liboptions.get(this.options()),
-                filter = libfilter.get(grunt, options);
+            // write to global options
+            options = this.options(options);
 
-            grunt.verbose.writeflags(options, 'Options');
+            template.init('wrapConfig', options.configWrapper);
+            template.init('wrapModule', options.moduleWrapper);
 
-            grunt.verbose.writeln('Reading configs from %s files', this.files.length);
-
-            /**
-            * Run the program.
-            */
-            if (this.files && this.files.length > 0) {
-                this.files.forEach(function(file) {
-                    // store the meta data in the modules object.
-                    var metaConfig = getMetaConfig.call(this, file.src);
-
-                    // Write joined contents to destination filepath.
-                    grunt.file.write(file.dest, JSON.stringify(metaConfig, null, options.space));
-                }, this);
-            }
-            else {
-                grunt.verbose.writeln('Failed to find files');
-            }
-        }
-    );
-
-    grunt.registerMultiTask(
-        'yui-config',
-        'Generates a custom YUI group config.',
-        function() {
-            var options = liboptions.get(this.options()),
-                precompiler = libprecompile.get(grunt, options),
-                filter = libfilter.get(grunt, options);
-
-            grunt.verbose.writeflags(options, 'Options');
-
-            grunt.verbose.writeln('Reading configs from %s files', this.files.length);
-
-            /**
-            * Run the program.
-            */
-            if (this.files && this.files.length > 0) {
-                this.files.forEach(function(file) {
-                    var meta,
-                        configContent;
-
-                    // store the meta data in the modules object.
-                    meta = file.src.map(function(filepath) {
-                        return grunt.file.read(filepath);
-                    }, this).join('');
-
-                    // wrap meta in the config.
-                    configContent = precompiler.wrap({
-                        meta: meta
-                    });
-
-                    // Write joined contents to destination filepath.
-                    grunt.file.write(file.dest, configContent);
-                }, this);
-            }
-            else {
-                grunt.verbose.writeln('Failed to find files');
-            }
-        }
-    );
-
-    grunt.registerMultiTask(
-        'yui-build',
-        'Builds each module',
-        function() {
-            var options = liboptions.get(this.options()),
-                precompiler = libprecompile.get(grunt, options),
-                filter = libfilter.get(grunt, options),
-                modules = {};
-
-            grunt.verbose.writeflags(options, 'Options');
-
-            grunt.verbose.writeln('Reading configs from %s files', this.files.length);
-
-            /**
-            * Run the program.
-            */
-            if (this.files && this.files.length > 0) {
-                this.files.forEach(function(file) {
-
-                    file.src.forEach(function(buildFile) {
-                        var build;
-
-                        buildFile = libpath.join(file.cwd, buildFile);
-
-                        grunt.log.writeln('Reading build: %s', buildFile);
-                        build = JSON.parse(grunt.file.read(buildFile));
-
-                        // loop through modules to build
-                        Object.keys(build.builds).forEach(function(moduleName) {
-                            var content = [],
-                                destPath;
-
-                            // join the js files.
-                            build.builds[moduleName].jsfiles.forEach(function(jsfile) {
-                                var data = grunt.file.read(libpath.join(libpath.dirname(buildFile), 'js', jsfile));
-                                grunt.log.writeln('Reading module: %s', jsfile);
-                                content.push(data);
-                            });
-
-                            destPath = libpath.join(file.dest, moduleName, moduleName + '.js');
-
-                            // Write joined contents to destination filepath.
-                            grunt.log.writeln('Writing file: %s', destPath);
-                            grunt.file.write(destPath, content.join('\n'));
-                        });
-                    }, this);
-                }, this);
-            }
-            else {
-                grunt.verbose.writeln('Failed to find files');
-            }
+            grunt.task.run('yui-config');
         }
     );
 };
