7f472494ea9c5ef2223c2091e7c92be9f56782a1
Bret K. Ikehara <bret.k.ikehara@gmail.com>
2014-03-27 01:45:25 -1000

    Added tests for the config.find function.

--- tests/test-02-parse-config.js
+++ tests/test-02-parse-config.js
@@ -43,6 +43,22 @@ module.exports = {
 
 		callback();
 	},
+	// finds all the meta files.
+	findMetaFiles: function(test) {
+		var metaFiles = libyui.config.find(libyui.options),
+			actual;
+
+		actual = checkArrays(metaFiles, [
+			'star-widget/meta/star-overlay.json',
+			'star-widget/meta/star-panel.json',
+			'star-widget/meta/star-tooltip.json',
+			'star-widget-plugin/meta/star-plugin.json'
+		]);
+
+		test.ok(actual, "Found all 4 meta files");
+
+		test.done();
+	},
 	// ensure that the written config will load the modules.
 	checkAvailable: function(test) {
 		// check the whether the templates worked.
